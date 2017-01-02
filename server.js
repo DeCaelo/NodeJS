b2ac850483c99e0aa32491283a4b62a0a418028a
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2017-01-02 17:28:03 +0100

    add folder with mkdir

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var fs = require('fs');
 
-var content = `
-Lorem ipsum dolor sit amet, consectetur adipisicing elit. In adipisci sint reiciendis, corrupti, soluta nisi numquam officiis vero magni illo eaque doloremque eveniet quo nesciunt! Non nesciunt, nemo ut necessitatibus?
-`;
-
-fs.writeFile('test.txt', content.trim(), (error, file) => {
-    console.log('File created');
-});
+if (fs.existsSync('lib')) {
+    console.log('Dir exist');
+} else {
+    fs.mkdir('lib', (error) => {
+        console.log('Dir created');
+    });
+}
