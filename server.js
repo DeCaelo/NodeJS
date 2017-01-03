68272a03bff68723e92cec4b15c3f29cf272e23f
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2017-01-03 10:50:18 +0100

    static files in public folder out server.js

--- server.js
+++ server.js
@@ -1,21 +1,28 @@
 var fs = require('fs');
 var http = require('http');
+var path = require('path');
 
 var server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/html'});
+    console.log(`${req.method} : request for ${req.url}`);
 
-    var responseBody = `
-        <html>
-            <head></head>
-            <body>
-            <h1>Je suis un serveur HTTP</h1>
-            <p>${req.url}</p>
-            <p>${req.method}</p>
-            </body>
-        </html>
-    `;
+    if (req.url === '/') {
+        fs.readFile('./public/index.html', 'UTF-8', (err, html) => {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(html);
+        });
+    } else if (req.url.match(/.css$/)) {
+        var cssPath = path.join(__dirname, 'public', req.url);
+        var fileStream = fs.createReadStream(cssPath, 'UTF-8');
 
-    res.end(responseBody);
+        res.writeHead(200, {'Content-Type': 'text/css'});
+
+        fileStream.pipe(res);
+    } else {
+        res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.end('404 not found');
+    }
+
+
 });
 
 server.listen(3000);
